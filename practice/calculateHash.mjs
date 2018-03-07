1ef26b94a4ad9b10e7c363e4ad726679a059c951
Clet Simon Sevillejo <cssevillejo@gmail.com>
2018-03-07 10:20:45 +0900

    practice creating a blockchain

--- practice/calculateHash.mjs
+++ practice/calculateHash.mjs
@@ -5,7 +5,7 @@
 // Hash function
 // https://en.wikipedia.org/wiki/Hash_function
 // (data1, data2, data3, data4) => hash value
-// 
+//
 // crypto-js
 // https://github.com/brix/crypto-js
 
@@ -18,22 +18,22 @@ const calculateHash = (
 	timestamp,
 	data
 ) => {
-	// return CryptoJS.SHA256(
-	// 	index
-	//   + previousHash
-	//   + timestamp
-	//   + data
-	// ).toString();
+	return CryptoJS.SHA256(
+		index
+	  + previousHash
+	  + timestamp
+	  + data
+	).toString();
 };
 
 // Calculate hash value for a block
 const calculateHashForBlock = (block) => {
-	// return calculateHash(
-	// 	block.index,
-	// 	block.previousHash,
-	// 	block.timestamp,
-	// 	block.data
-	// );
+	return calculateHash(
+		block.index,
+		block.previousHash,
+		block.timestamp,
+		block.data
+	);
 };
 
 export {
